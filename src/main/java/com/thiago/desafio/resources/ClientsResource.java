289d6342c61291479e5b55750119247ca7bfe471
Thiago Gomes <thiagofg.eng@gmail.com>
2021-09-25 23:47:59 -0300

    added crossorigin annotation

--- src/main/java/com/thiago/desafio/resources/ClientsResource.java
+++ src/main/java/com/thiago/desafio/resources/ClientsResource.java
@@ -24,6 +24,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/clients")
+@CrossOrigin(*)
 public class ClientsResource {
     
     @Autowired
