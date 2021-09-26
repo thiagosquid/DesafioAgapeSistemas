3d3646e861b2140bdf27161802a28659938df50f
Thiago Gomes <thiagofg.eng@gmail.com>
2021-09-25 23:53:12 -0300

    remove cross origin

--- src/main/java/com/thiago/desafio/resources/UsersResource.java
+++ src/main/java/com/thiago/desafio/resources/UsersResource.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
-@CrossOrigin(*)
+// @CrossOrigin(*)
 public class UsersResource {
     
     @Autowired
