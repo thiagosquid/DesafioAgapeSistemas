0ade991c135ce6a15e3286d480011b9641a12a2a
Thiago Gomes <thiagofg.eng@gmail.com>
2021-09-25 23:48:22 -0300

    added crossorigin annotation

--- src/main/java/com/thiago/desafio/resources/UsersResource.java
+++ src/main/java/com/thiago/desafio/resources/UsersResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
+@CrossOrigin(*)
 public class UsersResource {
     
     @Autowired
