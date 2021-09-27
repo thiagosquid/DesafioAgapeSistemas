dbc044051928c94f9405775ea4256fe32af0508e
Thiago Gomes <thiagofg.eng@gmail.com>
2021-09-27 11:16:48 -0300

    Update CorsConfiguration.java

--- src/main/java/com/thiago/desafio/config/CorsConfiguration.java
+++ src/main/java/com/thiago/desafio/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(*)
+            .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
